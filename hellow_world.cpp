ee21b1f643af24293f3fca9a6a7a619f4c6be3cf
Emile Tenezakis <e.tenezakis@gmail.com>
2020-06-14 23:11:48 -0500

    wow....

---
+++ hellow_world.cpp
@@ -0,0 +1,12 @@
+// hello world code
+
+#include <iostream>
+
+int main(){
+
+	std::string quote;
+	std::cout << "please say something ! I want to hear your voice !\n";
+	std::cin >> quote;
+	std::cout << quote << "\n" << "Wow... that's impressive.... \n";
+
+}
